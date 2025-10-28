fff0aa906b65579ed4111e39dffcaf60367f1071
WhiteJbb <gunni6112@gmail.com>
2025-10-28 15:11:00 +0900

    Fix : build 문제 해결

---
+++ app/src/main/java/com/example/dronedilivery/model/Site.java
@@ -0,0 +1,13 @@
+package com.example.dronedilivery.model;
+
+public class Site {
+    private final String name;
+
+    public Site(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
